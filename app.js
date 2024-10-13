46a27cbf3b896f7a708552ae6863ec4e839859bb
HemantGayakwad <hemantgaykwad555@gmail.com>
2024-10-13 05:45:47 -0700

    add initial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+app.engine("ejs", ejsMate)
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.listen(3000,()=>{
+    console.log("app is listening on port 3000");
+});
+
+app.get("/",(req,res)=>{
+    res.send("This is home page");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home.ejs")
+});
