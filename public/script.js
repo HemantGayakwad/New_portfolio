192d3a6beca05d26881d9eee57bc5fbc8f3235cd
HemantGayakwad <hemantgaykwad555@gmail.com>
2024-11-08 01:04:44 -0800

    gsap applied with scroll trigger

--- public/script.js
+++ public/script.js
@@ -1,40 +1,181 @@
-function marqueAnimation(){
-    window.addEventListener("wheel",function(dets){
-        if(dets.deltaY>0)
-        {
-            gsap.to(".marque",{
-                transform:'translateX(-200%)',
-                duration:6,
-                repeat:-1,
-                ease:"none"
-            })
-        }else{
-            gsap.to(".marque",{
-                transform:'translateX(0%)',
-                duration:4,
-                repeat:-1,
-                ease:"none"
-            })
-        }
-    })
-}
+// function marqueAnimation(){
+//     window.addEventListener("wheel",function(dets){
+//         if(dets.deltaY>0)
+//         {
+//             gsap.to(".marque",{
+//                 transform:'translateX(-200%)',
+//                 duration:6,
+//                 repeat:-1,
+//                 ease:"none"
+//             })
+//         }else{
+//             gsap.to(".marque",{
+//                 transform:'translateX(0%)',
+//                 duration:4,
+//                 repeat:-1,
+//                 ease:"none"
+//             })
+//         }
+//     })
+// }
 
-marqueAnimation()
+// marqueAnimation()
 
-gsap.from("h1",{
+// gsap.from("h1",{
+//     opacity:0,
+//     duration:1,
+//     y:30,
+// })
+// gsap.from("h3",{
+//     opacity:0,
+//     duration:1,
+//     y:30,
+//     stagger:0.2
+// })
+// gsap.from("p",{
+//     opacity:0,
+//     duration:1,
+//     y:30,
+//     stagger:0.2
+// })
+gsap.to(".marque",{
+    transform:'translateX(-80%)',
+    duration:2,
+    scrollTrigger:{
+        trigger:".marque",
+        scroller:"body",
+        start: 'top 80%',
+        end:'top 20%',
+        scrub:3
+    }
+})
+  
+gsap.from(".page4 h3",{
     opacity:0,
     duration:1,
     y:30,
+    scrollTrigger:{
+        trigger:'.page4 h3',
+        scroller:'body',
+        start:'top 70%',
+        end:'top 30%',
+        scrub:2
+    }
 })
-gsap.from("h3",{
+
+gsap.from(".aboutlinks a",{
     opacity:0,
     duration:1,
     y:30,
-    stagger:0.2
+    scrollTrigger:{
+        trigger:'.aboutlinks a',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 40%',
+        scrub:2
+    }
 })
-gsap.from("p",{
+
+gsap.from(".about h3",{
+    opacity:0,
+    duration:1,
+    x:30,
+    scrollTrigger:{
+        trigger:'.about h3',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 50%',
+        scrub:2
+    }
+})
+
+gsap.from(".projects h3",{
+    opacity:0,
+    duration:1,
+    x:30,
+    scrollTrigger:{
+        trigger:'.projects h3',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 50%',
+        scrub:2
+    }
+})
+
+gsap.from(".cc img",{
     opacity:0,
     duration:1,
     y:30,
-    stagger:0.2
+    scrollTrigger:{
+        trigger:'.cc img',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 60%',
+        scrub:2
+    }
 })
+
+gsap.from(".btns a",{
+    opacity:0,
+    duration:1,
+    x:30,
+    scrollTrigger:{
+        trigger:'.btns a',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 60%',
+        scrub:2
+    }
+})
+
+gsap.from(".page1 h1",{
+    opacity:0,
+    duration:1,
+    x:30  
+})
+
+gsap.from(".page1 img",{
+    opacity:0,
+    duration:1,
+    y:30  
+})
+
+gsap.from(".content h3",{
+    opacity:0,
+    duration:1,
+    y:30,
+    scrollTrigger:{
+        trigger:'.content h3',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 50%',
+        scrub:2
+    }
+})
+
+gsap.from(".heading2 h3",{
+    opacity:0,
+    duration:1,
+    x:30,
+    scrollTrigger:{
+        trigger:'.heading2 h3',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 50%',
+        scrub:2
+    }
+})
+
+gsap.from(".heading2 p",{
+    opacity:0,
+    duration:1,
+    y:30,
+    scrollTrigger:{
+        trigger:'.heading2 p',
+        scroller:'body',
+        start:'top 80%',
+        end:'top 50%',
+        scrub:2
+    }
+})
+
